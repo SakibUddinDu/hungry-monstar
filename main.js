028a947fe6e701abb240e86194ba7ddd7d4b2816
Mainuddinhasansakib <sakibuddin831@gmail.com>
2021-02-07 11:09:51 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+// const searchBtn = document.getElementById('button');
+
+
+// = document.getElementById('')
+function displayFoods() {
+    fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata')
+    .then(res => res.json())
+    .then(json=> )
+
+    
+    // .then(response => response.json())
+    // .then(json => console.log(json))
+    
+    
+}
